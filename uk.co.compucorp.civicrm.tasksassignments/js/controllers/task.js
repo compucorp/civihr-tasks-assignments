4938a140eebc8d75ddd9fadf3852348383f5c24a
Mateusz Cieplinski <mateusz@coldrun.pl>
2015-03-11 14:19:00 +0100

    new design

--- uk.co.compucorp.civicrm.tasksassignments/js/controllers/task.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/controllers/task.js
@@ -1,1 +1,1 @@
-define(['controllers/controllers'], function(controllers){    controllers.controller('TaskCtrl',['$scope', '$log',        function($scope, $log){            $log.debug('Controller: TaskCtrl');            $scope.isCollapsed = true;        }]);});
+define(['controllers/controllers'], function(controllers){    controllers.controller('TaskCtrl',['$scope', '$log',        function($scope, $log){            $log.debug('Controller: TaskCtrl');            $scope.isCollapsed = true;        }]);});
