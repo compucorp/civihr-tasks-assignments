ef3ce2789ceaf7b0b2a3f86f52cfbf14bf70a131
coldrunKacper <kacper@coldrun.pl>
2015-05-28 15:04:51 +0200

    Fixing contact URL for daily reminder.

--- uk.co.compucorp.civicrm.tasksassignments/CRM/Tasksassignments/Reminder.php
+++ uk.co.compucorp.civicrm.tasksassignments/CRM/Tasksassignments/Reminder.php
@@ -209,7 +209,7 @@ class CRM_Tasksassignments_Reminder
             {
                 list($type, $cId, $cSortName) = explode(':', $value);
                 
-                $contactUrl = CRM_Report_Utils_Report::getNextUrl('contact/detail', 'reset=1&force=&id_op=eq&id_value=' . $cId);
+                $contactUrl = '/civicrm/contact/view?reset=1&cid=' . $cId;
                 $activityContact[$type][$cId] = '<a href="' . $contactUrl . '">' . $cSortName . '</a>';;
             }
             
