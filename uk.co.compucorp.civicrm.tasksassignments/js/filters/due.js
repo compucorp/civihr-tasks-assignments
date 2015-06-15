63cd759db40b3ce1b64be941852ce610f851af6a
Mateusz Cieplinski <mateusz@coldrun.pl>
2015-06-15 16:02:07 +0200

    Documents filter by exp date

--- uk.co.compucorp.civicrm.tasksassignments/js/filters/due.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/filters/due.js
@@ -20,7 +20,8 @@ define(['filters/filters'], function(filters){
             switch(type){
                 case 'overdue':
                     for (i; i < inputArrlen; i++) {
-                        if (new Date(inputArr[i].activity_date_time).setHours(0, 0, 0, 0) < today) {
+                        if (new Date(inputArr[i].activity_date_time).setHours(0, 0, 0, 0) < today ||
+                           (inputArr[i].expire_date && new Date(inputArr[i].expire_date).setHours(0, 0, 0, 0) < today)) {
                             filteredArr.push(inputArr[i]);
                         }
                     }
@@ -33,12 +34,15 @@ define(['filters/filters'], function(filters){
                     }
 
                     var itemDateTime,
+                        itemDateExp,
                         filterDateTimeFrom = dateRange.from ? new Date(dateRange.from).setHours(0, 0, 0, 0) : -Infinity,
                         filterDateTimeUntil = dateRange.until ? new Date(dateRange.until).setHours(0, 0, 0, 0) : Infinity;
 
                     for (i; i < inputArrlen; i++) {
                         itemDateTime = new Date(inputArr[i].activity_date_time).setHours(0, 0, 0, 0);
-                        if (itemDateTime >= filterDateTimeFrom && itemDateTime <= filterDateTimeUntil) {
+                        itemDateExp = inputArr[i].expire_date ? new Date(inputArr[i].expire_date).setHours(0, 0, 0, 0) : false;
+                        if ((itemDateTime >= filterDateTimeFrom && itemDateTime <= filterDateTimeUntil) ||
+                            (itemDateExp && itemDateExp >= filterDateTimeFrom && itemDateExp <= filterDateTimeUntil)) {
                             filteredArr.push(inputArr[i]);
                         }
                     }
@@ -46,7 +50,8 @@ define(['filters/filters'], function(filters){
                     break;
                 case 'dueToday':
                     for (i; i < inputArrlen; i++) {
-                        if (new Date(inputArr[i].activity_date_time).setHours(0, 0, 0, 0) == today) {
+                        if (new Date(inputArr[i].activity_date_time).setHours(0, 0, 0, 0) == today ||
+                           (inputArr[i].expire_date && new Date(inputArr[i].expire_date).setHours(0, 0, 0, 0) == today)) {
                             filteredArr.push(inputArr[i]);
                         }
                     }
@@ -58,12 +63,15 @@ define(['filters/filters'], function(filters){
                         diff = d.getDate() - day + (day == 0 ? -6:1),
                         mon = new Date(d.setDate(diff)),
                         sun = new Date(d.setDate(mon.getDate()+7)),
-                        itemDateTime;
+                        itemDateTime,
+                        itemDateExp;
 
                     for (i; i < inputArrlen; i++) {
-                        itemDateTime = new Date(inputArr[i].activity_date_time).setHours(0, 0, 0, 0)
+                        itemDateTime = new Date(inputArr[i].activity_date_time).setHours(0, 0, 0, 0);
+                        itemDateExp = inputArr[i].expire_date ? new Date(inputArr[i].expire_date).setHours(0, 0, 0, 0) : false;
 
-                        if (itemDateTime >= mon.setHours(0, 0, 0, 0) && itemDateTime < sun.setHours(0, 0, 0, 0)) {
+                        if ((itemDateTime >= mon.setHours(0, 0, 0, 0) && itemDateTime < sun.setHours(0, 0, 0, 0)) ||
+                            (itemDateExp && itemDateExp >= mon.setHours(0, 0, 0, 0) && itemDateExp < sun.setHours(0, 0, 0, 0))) {
                             filteredArr.push(inputArr[i]);
                         }
                     }
