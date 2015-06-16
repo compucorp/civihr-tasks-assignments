511b65c49c7927aa54a8812764532ea76ad37ae0
Mateusz Cieplinski <mateusz@coldrun.pl>
2015-06-16 17:26:38 +0200

    Build script

--- uk.co.compucorp.civicrm.tasksassignments/build.js
+++ uk.co.compucorp.civicrm.tasksassignments/build.js
@@ -3,12 +3,12 @@
     name: "ta-main",
     out: "dist/ta-main.js",
     removeCombined: true,
+    //optimize: "none",
     paths: {
         'angularSelect': 'vendor/angular/select',
         'angularBootstrapCalendar': 'vendor/angular/angular-bootstrap-calendar-tpls-custom',
         'angularChecklistModel': 'vendor/angular/checklist-model',
         'angularRouter': 'vendor/angular/angular-ui-router',
-        'crmUi': 'vendor/angular/crmUi',
         'moment': 'vendor/moment.min',
         'requireLib': 'empty:',
         'textAngular': 'vendor/angular/textAngular.min',
