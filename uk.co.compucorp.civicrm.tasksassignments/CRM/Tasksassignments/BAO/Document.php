d5ac2eb7198855accf0c50db015a0ef465152afc
coldrunKacper <kacper@coldrun.pl>
2015-06-25 17:59:26 +0200

    Setting default Task / Document status on Create action.

--- uk.co.compucorp.civicrm.tasksassignments/CRM/Tasksassignments/BAO/Document.php
+++ uk.co.compucorp.civicrm.tasksassignments/CRM/Tasksassignments/BAO/Document.php
@@ -14,6 +14,11 @@ class CRM_Tasksassignments_BAO_Document extends CRM_Tasksassignments_DAO_Documen
         $hook = empty($params['id']) ? 'create' : 'edit';
         CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
         
+        if (empty($params['status_id']) && $hook === 'create')
+        {
+            $params['status_id'] = 1;
+        }
+        
         $instance = parent::create($params);
         CRM_Tasksassignments_Reminder::sendReminder((int)$instance->id);
         
