9ab90aa4babca47e52187587d124ff102a154534
Krzysztof Kałamarski <kkalamarski@gmail.com>
2016-01-07 11:13:27 +0000

    PCHR-478: Display for Modal with predefined contact.

--- uk.co.compucorp.civicrm.tasksassignments/js/controllers/task.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/controllers/task.js
@@ -1,32 +1,32 @@
-define(['controllers/controllers',
-        'moment',
-        'services/task'], function(controllers, moment){
-    controllers.controller('TaskCtrl',['$scope', '$log', '$rootScope',
-        function($scope, $log, $rootScope){
+define([
+    'controllers/controllers',
+    'moment',
+    'services/task'
+], function (controllers, moment) {
+    controllers.controller('TaskCtrl', ['$scope', '$log', '$rootScope',
+        function ($scope, $log, $rootScope) {
             $log.debug('Controller: TaskCtrl');
 
             $scope.isCollapsed = true;
-            $scope.picker = { opened: false };
+            $scope.picker = {opened: false};
             $scope.task.activity_date_time = !!$scope.task.activity_date_time ? moment($scope.task.activity_date_time).toDate() : null;
 
-            $scope.dpOpen = function($event){
+            $scope.dpOpen = function ($event) {
                 $event.preventDefault();
                 $event.stopPropagation();
-
                 $scope.picker.opened = true;
-
             };
 
-            $scope.$watch('task.status_id',function(statusId){
+            $scope.$watch('task.status_id', function (statusId) {
                 var isResolved = $rootScope.cache.taskStatusResolve.indexOf(statusId) > -1;
-
                 $scope.task.resolved = isResolved;
                 $scope.task.completed = isResolved;
             });
 
-            $scope.$watch('task.activity_date_time',function(taskDateTime){
+            $scope.$watch('task.activity_date_time', function (taskDateTime) {
                 $scope.task.due = new Date(taskDateTime).setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0);
             });
 
-        }]);
+        }
+    ]);
 });
