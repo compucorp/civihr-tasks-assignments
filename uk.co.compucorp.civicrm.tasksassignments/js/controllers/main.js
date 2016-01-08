6ef14bfb67df0c2d09502dc52247c99c35e57452
Krzysztof Kałamarski <kkalamarski@gmail.com>
2016-01-08 10:45:33 +0000

    PCHR-444: Refactoring Javascript

--- uk.co.compucorp.civicrm.tasksassignments/js/controllers/main.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/controllers/main.js
@@ -9,25 +9,25 @@ define([
             $rootScope.modalDocument = function (data, e) {
                 e && e.preventDefault();
 
-                var data = data || {},
-                    modalInstance = $modal.open({
-                        targetDomEl: $rootElement.find('div').eq(0),
-                        templateUrl: config.path.TPL + 'modal/document.html?v=3',
-                        controller: 'ModalDocumentCtrl',
-                        resolve: {
-                            data: function () {
-                                return data;
-                            },
-                            files: function () {
+                data = data || {};
+                var modalInstance = $modal.open({
+                    targetDomEl: $rootElement.find('div').eq(0),
+                    templateUrl: config.path.TPL + 'modal/document.html?v=3',
+                    controller: 'ModalDocumentCtrl',
+                    resolve: {
+                        data: function () {
+                            return data;
+                        },
+                        files: function () {
 
-                                if (!data.id || !+data.file_count) {
-                                    return [];
-                                }
-
-                                return FileService.get(data.id, 'civicrm_activity')
+                            if (!data.id || !+data.file_count) {
+                                return [];
                             }
+
+                            return FileService.get(data.id, 'civicrm_activity')
                         }
-                    });
+                    }
+                });
 
                 modalInstance.result.then(function (results) {
                     $scope.$broadcast('documentFormSuccess', results, data);
@@ -37,46 +37,44 @@ define([
             };
 
             $rootScope.modalTask = function (data) {
-                var data = data || {},
-                    modalInstance = $modal.open({
-                        targetDomEl: $rootElement.find('div').eq(0),
-                        templateUrl: config.path.TPL + 'modal/task.html?v=5',
-                        controller: 'ModalTaskCtrl',
-                        resolve: {
-                            data: function () {
-                                return data;
-                            }
+                data = data || {};
+                var modalInstance = $modal.open({
+                    targetDomEl: $rootElement.find('div').eq(0),
+                    templateUrl: config.path.TPL + 'modal/task.html?v=5',
+                    controller: 'ModalTaskCtrl',
+                    resolve: {
+                        data: function () {
+                            return data;
                         }
-                    });
+                    }
+                });
 
                 modalInstance.result.then(function (results) {
                     $scope.$broadcast('taskFormSuccess', results, data);
                 }, function () {
                     $log.info('Modal dismissed');
                 });
-
             };
 
             $rootScope.modalAssignment = function (data) {
-                var data = data || {},
-                    modalInstance = $modal.open({
-                        targetDomEl: $rootElement.find('div').eq(0),
-                        templateUrl: config.path.TPL + 'modal/assignment.html?v=3',
-                        controller: 'ModalAssignmentCtrl',
-                        size: 'lg',
-                        resolve: {
-                            data: function () {
-                                return data;
-                            }
+                data = data || {};
+                var modalInstance = $modal.open({
+                    targetDomEl: $rootElement.find('div').eq(0),
+                    templateUrl: config.path.TPL + 'modal/assignment.html?v=3',
+                    controller: 'ModalAssignmentCtrl',
+                    size: 'lg',
+                    resolve: {
+                        data: function () {
+                            return data;
                         }
-                    });
+                    }
+                });
 
                 modalInstance.result.then(function (results) {
                     $scope.$broadcast('assignmentFormSuccess', results, data);
                 }, function () {
                     $log.info('Modal dismissed');
                 });
-
             };
 
             $rootScope.modalReminder = function (data, type) {
@@ -99,7 +97,6 @@ define([
                     }
                 });
             };
-
         }
     ]);
 });
