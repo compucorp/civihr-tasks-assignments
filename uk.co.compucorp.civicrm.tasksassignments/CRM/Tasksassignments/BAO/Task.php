57e6ebd5c4c9444ef1a32b39934a337ca759d2f9
Paweł Łuczkiewicz <pawel@compucorp.co.uk>
2016-01-13 12:22:01 +0000

    PCHR-635: Changed code-formatting

--- uk.co.compucorp.civicrm.tasksassignments/CRM/Tasksassignments/BAO/Task.php
+++ uk.co.compucorp.civicrm.tasksassignments/CRM/Tasksassignments/BAO/Task.php
@@ -1,48 +1,43 @@
 <?php
 
-class CRM_Tasksassignments_BAO_Task extends CRM_Tasksassignments_DAO_Task
-{
-    /**
-     * Create a new Task based on array-data
-     *
-     * @param array $params key-value pairs
-     * @return CRM_Tasksassignments_DAO_Task|NULL
-     */
-    public static function create(&$params)
-    {
-        $entityName = 'Task';
-        $hook = empty($params['id']) ? 'create' : 'edit';
-        CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
-        
-        if (empty($params['status_id']) && $hook === 'create')
-        {
-            $params['status_id'] = 1;
-        }
+class CRM_Tasksassignments_BAO_Task extends CRM_Tasksassignments_DAO_Task {
+  /**
+   * Create a new Task based on array-data
+   *
+   * @param array $params key-value pairs
+   * @return CRM_Tasksassignments_DAO_Task|NULL
+   */
+  public static function create(&$params) {
+    $entityName = 'Task';
+    $hook = empty($params['id']) ? 'create' : 'edit';
+    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
 
-        $currentInstance = new static();
-        $currentInstance->get('id', $params['id']);
-
-        $activityContactResult = civicrm_api3('ActivityContact', 'get', array(
-          'sequential' => 1,
-          'activity_id' => $params['id'],
-        ));
-
-        $previousAssigneeId = null;
-
-        foreach($activityContactResult['values'] as $value)
-        {
-            if($value['record_type_id'] == 1) // 1 is assignee
-            {
-                $previousAssigneeId = $value['contact_id'];
-                break;
-            }
-        }
-
-        $instance = parent::create($params);
-        CRM_Tasksassignments_Reminder::sendReminder((int)$instance->id, null, false, $previousAssigneeId);
-        
-        CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
-        
-        return $instance;
+    if (empty($params['status_id']) && $hook === 'create') {
+      $params['status_id'] = 1;
     }
+
+    $currentInstance = new static();
+    $currentInstance->get('id', $params['id']);
+
+    $activityContactResult = civicrm_api3('ActivityContact', 'get', array(
+      'sequential' => 1,
+      'activity_id' => $params['id'],
+    ));
+
+    $previousAssigneeId = NULL;
+
+    foreach ($activityContactResult['values'] as $value) {
+      if ($value['record_type_id'] == 1) { // 1 is assignee
+        $previousAssigneeId = $value['contact_id'];
+        break;
+      }
+    }
+
+    $instance = parent::create($params);
+    CRM_Tasksassignments_Reminder::sendReminder((int) $instance->id, NULL, FALSE, $previousAssigneeId);
+
+    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
+
+    return $instance;
+  }
 }
