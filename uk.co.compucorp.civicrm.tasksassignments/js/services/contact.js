2655a73355bf2ebab4c98d4ef67bee5a6c84eff3
Krzysztof Kałamarski <kkalamarski@gmail.com>
2016-01-14 13:29:03 +0200

    PCHR-778: Save And New Functionality + changed way of getting contacts to angular app.

--- uk.co.compucorp.civicrm.tasksassignments/js/services/contact.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/services/contact.js
@@ -1,101 +1,114 @@
 define(['services/services',
-        'services/utils'], function (services) {
+    'services/utils'], function (services) {
 
-    services.factory('ContactService', ['$resource', 'config', '$q', '$filter', '$rootScope','UtilsService','$log',
+    services.factory('ContactService', ['$resource', 'config', '$q', '$filter', '$rootScope', 'UtilsService', '$log',
         function ($resource, config, $q, $filter, $rootScope, UtilsService, $log) {
             $log.debug('Service: ContactService');
 
-        var Contact = $resource(config.url.REST, {
-            action: 'get',
-            entity: 'contact',
-            json: {}
-        });
+            var Contact = $resource(config.url.REST, {
+                action: 'get',
+                entity: 'contact',
+                json: {}
+            });
 
-        return {
-            get: function(id) {
+            return {
+                get: function (id) {
 
-                if (!id || (typeof +id !== 'number' && typeof id !== 'object') ) {
-                    return null
-                }
-
-                var deferred = $q.defer();
-
-                Contact.get({
-                    'json': {
-                        'id': id,
-                        'return': 'display_name, sort_name, id, contact_id, contact_type, email',
-                        'debug': config.DEBUG
-                }}, function(data){
-
-                    if (UtilsService.errorHandler(data,'Unable to fetch contacts',deferred)) {
-                        return
+                    if (!id || (typeof +id !== 'number' && typeof id !== 'object')) {
+                        return null
                     }
 
-                    deferred.resolve(data.values);
-                },function(){
-                    deferred.reject('Unable to fetch contact');
-                });
+                    var deferred = $q.defer();
 
-                return deferred.promise;
-            },
-            search: function(input, params){
+                    Contact.get({
+                        'json': {
+                            'id': id,
+                            'return': 'display_name, sort_name, id, contact_id, contact_type, email',
+                            'debug': config.DEBUG
+                        }
+                    }, function (data) {
 
-                if (params && typeof params !== 'object') {
-                    return null
-                }
+                        if (UtilsService.errorHandler(data, 'Unable to fetch contacts', deferred)) {
+                            return
+                        }
 
-                var deferred = $q.defer(),
-                    params = params || {};
-
-                Contact.get({
-                    action: 'getlist',
-                    json: {
-                        input: input,
-                        params: params,
-                        debug: config.DEBUG
-                    }}, function(data){
-
-                    if (UtilsService.errorHandler(data,'Unable to fetch contact list',deferred)) {
-                        return
-                    }
-
-                    deferred.resolve(data.values);
-
-                },function(){
-                    deferred.reject('Unable to fetch contact list');
-                });
-
-                return deferred.promise;
-            },
-            updateCache: function(data){
-                var obj = $rootScope.cache.contact.obj || {}, arr = [], arrSearch = [], contact, contactId;
-
-                angular.extend(obj,data);
-
-                for (contactId in obj) {
-                    contact = obj[contactId];
-
-                    arr.push(contact);
-
-                    arrSearch.push({
-                        description: contact.email ? [contact.email] : [],
-                        label: contact.display_name,
-                        icon_class: contact.contact_type,
-                        id: contact.contact_id
+                        deferred.resolve(data.values);
+                    }, function () {
+                        deferred.reject('Unable to fetch contact');
                     });
+
+                    return deferred.promise;
+                },
+                search: function (input, params) {
+
+                    if (params && typeof params !== 'object') {
+                        return null
+                    }
+
+                    var deferred = $q.defer(),
+                        params = params || {};
+
+                    Contact.get({
+                        action: 'get',
+                        json: {
+                            "contact_type": "Individual",
+                            display_name: input,
+                            params: params,
+                            debug: config.DEBUG,
+                            return: 'display_name, contact_id, contact_type, email'
+                        }
+                    }, function (data) {
+                        if (UtilsService.errorHandler(data, 'Unable to fetch contact list', deferred)) {
+                            return
+                        }
+
+                        var contacts = [];
+                        for(var i in data.values){
+                            contacts.push({
+                                label: data.values[i].display_name,
+                                description: [data.values[i].email],
+                                id: data.values[i].contact_id,
+                                icon_class: data.values[i].contact_type
+                            });
+                        }
+                        console.log(contacts);
+                        deferred.resolve(contacts);
+
+                    }, function () {
+                        deferred.reject('Unable to fetch contact list');
+                    });
+
+                    return deferred.promise;
+                },
+                updateCache: function (data) {
+                    var obj = $rootScope.cache.contact.obj || {}, arr = [], arrSearch = [], contact, contactId;
+
+                    angular.extend(obj, data);
+
+                    for (contactId in obj) {
+                        contact = obj[contactId];
+
+                        arr.push(contact);
+
+                        arrSearch.push({
+                            description: contact.email ? [contact.email] : [],
+                            label: contact.display_name,
+                            icon_class: contact.contact_type,
+                            id: contact.contact_id
+                        });
+                    }
+
+                    arr = $filter('orderBy')(arr, 'sort_name');
+                    arrSearch = $filter('orderBy')(arrSearch, 'label');
+
+                    $rootScope.cache.contact = {
+                        arr: arr,
+                        obj: obj,
+                        arrSearch: arrSearch
+                    };
                 }
-
-                arr = $filter('orderBy')(arr, 'sort_name');
-                arrSearch = $filter('orderBy')(arrSearch, 'label');
-
-                $rootScope.cache.contact = {
-                    arr: arr,
-                    obj: obj,
-                    arrSearch: arrSearch
-                };
             }
-        }
 
-    }]);
+        }]);
 
 });
