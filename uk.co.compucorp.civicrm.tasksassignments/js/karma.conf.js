8cb599ff7ee8780b7c4bf71905423d1e12ccea05
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-02-04 14:58:55 +0100

    PCHR-839: Add test-main.js + small changes to karma.conf.js

--- uk.co.compucorp.civicrm.tasksassignments/js/karma.conf.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/karma.conf.js
@@ -1,6 +1,7 @@
 module.exports = function (config) {
     var civicrmPath = '../../../../../';
     var civihrPath = 'tools/extensions/civihr/';
+    var extPath = 'tools/extensions/civihr_tasks/uk.co.compucorp.civicrm.tasksassignments/';
 
     config.set({
         basePath: civicrmPath,
@@ -15,35 +16,39 @@ module.exports = function (config) {
             'packages/jquery/plugins/select2/select2.js',
             'packages/jquery/plugins/jquery.blockUI.js',
             'js/Common.js',
+            'js/crm.ajax.js',
+
+            //
+            extPath + 'js/test/global-variables.js',
 
             // manual loading of requirejs as to avoid interference with the global dependencies above
-            civihrPath + 'uk.co.compucorp.civicrm.tasksassignments/node_modules/requirejs/require.js',
-            civihrPath + 'uk.co.compucorp.civicrm.tasksassignments/node_modules/karma-requirejs/lib/adapter.js',
+            extPath + 'node_modules/requirejs/require.js',
+            extPath + 'node_modules/karma-requirejs/lib/adapter.js',
 
             // all the common/ dependencies
             civihrPath + 'org.civicrm.reqangular/dist/reqangular.min.js',
 
             // the application modules
-            { pattern: civihrPath + 'uk.co.compucorp.civicrm.tasksassignments/js/src/tasks-assignments/**/*.js', included: false },
+            { pattern: extPath + 'js/src/tasks-assignments/**/*.js', included: false },
 
             // the mocked components files
-            { pattern: civihrPath + 'uk.co.compucorp.civicrm.tasksassignments/js/test/mocks/**/*.js', included: false },
+            { pattern: extPath + 'js/test/mocks/**/*.js', included: false },
 
             // the test files
-            { pattern: civihrPath + 'uk.co.compucorp.civicrm.tasksassignments/js/test/**/*_test.js', included: false },
+            { pattern: extPath + 'js/test/**/*_test.js', included: false },
 
             // angular templates
-            civihrPath + 'uk.co.compucorp.civicrm.tasksassignments/views/**/*.html',
+            extPath + 'views/**/*.html',
 
             // the requireJS config file that bootstraps the whole test suite
-            civihrPath + 'uk.co.compucorp.civicrm.tasksassignments/js/test/test-main.js'
+            extPath + 'js/test/test-main.js'
         ],
         exclude: [
-            civihrPath + 'uk.co.compucorp.civicrm.tasksassignments/js/src/tasks-assignments.js'
+            extPath + 'js/src/tasks-assignments.js'
         ],
         // Used to transform angular templates in JS strings
         preprocessors: (function (obj) {
-            obj[civihrPath + 'uk.co.compucorp.civicrm.tasksassignments/views/**/*.html'] = ['ng-html2js'];
+            obj[extPath + 'views/**/*.html'] = ['ng-html2js'];
             return obj;
         })({}),
         ngHtml2JsPreprocessor: {
