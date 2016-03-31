240b8cd69ff5a483ee753f24d88016bbd81620d0
Krzysztof Kałamarski <kkalamarski@gmail.com>
2016-03-31 13:55:32 +0300

    PCHR-478: Tests for bulk assign

---
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/controllers/modal/modal-assignment_test.js
@@ -0,0 +1,104 @@
+define([
+    'common/angular',
+    'mocks/assignment',
+    'common/angularMocks',
+    'tasks-assignments/app'
+], function (angular, Mock) {
+    'use strict';
+
+    describe('ModalAssignmentCtrl', function () {
+        var ctrl, modalInstance, scope, AssignmentService, TaskService, DocumentService, ContactService, HR_settings;
+
+        beforeEach(module('civitasks.appDashboard'));
+        beforeEach(inject(function ($controller, $rootScope) {
+            modalInstance = {
+                close: jasmine.createSpy('modalInstance.close'),
+                dismiss: jasmine.createSpy('modalInstance.dismiss'),
+                result: {
+                    then: jasmine.createSpy('modalInstance.result.then')
+                }
+            };
+
+            scope = $rootScope.$new();
+
+            AssignmentService = {};
+            TaskService = {};
+            DocumentService = {};
+            ContactService = {};
+            HR_settings = {
+                DATE_FORMAT: 'DD/MM/YYYY'
+            };
+
+            ctrl = $controller('ModalAssignmentCtrl', {
+                $scope: scope,
+                $modalInstance: modalInstance,
+                AssignmentService: AssignmentService,
+                TaskService: TaskService,
+                DocumentService: DocumentService,
+                ContactService: ContactService,
+                data: {},
+                config: {},
+                settings: {},
+                HR_settings: HR_settings
+            });
+        }));
+
+        it('Controller should be defined', function () {
+            expect(ctrl).toBeDefined()
+        });
+
+        describe('Bulk Assign', function(){
+            beforeEach(function(){
+                angular.extend(scope, Mock);
+            });
+
+            it('Test copyAssignee method', function(){
+                var list = scope.taskList;
+
+                list[0].assignee_contact_id = [2];
+
+                expect(list[1].assignee_contact_id).toEqual([]);
+                expect(list[2].assignee_contact_id).toEqual([]);
+
+                scope.copyAssignee(list);
+
+                expect(list[1].assignee_contact_id).toEqual([2]);
+                expect(list[2].assignee_contact_id).toEqual([2]);
+            });
+
+            it('Test copyDate method', function(){
+                var list = scope.taskList;
+
+                list[0].activity_date_time = '2015-05-05';
+
+                expect(list[1].activity_date_time).toEqual('');
+                expect(list[2].activity_date_time).toEqual('');
+
+                scope.copyDate(list);
+
+                expect(list[1].activity_date_time).toEqual('2015-05-05');
+                expect(list[2].activity_date_time).toEqual('2015-05-05');
+            });
+        });
+
+    });
+});
+
+
+/*
+
+ , $q, $log, $filter, AssignmentService, TaskService, DocumentService, ContactService, HR_settings
+
+ $rootScope: $rootScope,
+ $q: $q,
+ $log: $log,
+ $filter: $filter,
+ AssignmentService: AssignmentService,
+ TaskService: TaskService,
+ DocumentService: DocumentService,
+ ContactService: ContactService,
+ data: {},
+ config: {},
+ settings: {},
+ HR_settings: HR_settings
+ */
