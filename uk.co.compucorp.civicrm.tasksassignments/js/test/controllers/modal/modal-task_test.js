12c7a108b21da21842ae46b5c5a45b09708828b9
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-04-20 11:21:30 +0200

    PCHR-983: Fix cache on task modal

--- uk.co.compucorp.civicrm.tasksassignments/js/test/controllers/modal/modal-task_test.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/controllers/modal/modal-task_test.js
@@ -7,46 +7,25 @@ define([
     'use strict';
 
     describe('ModalTaskCtrl', function () {
-        var ctrl, modalInstance, $scope, HR_settings, data, initController;
-
-        function fakeModalInstance() {
-            return {
-                close: jasmine.createSpy('modalInstance.close'),
-                dismiss: jasmine.createSpy('modalInstance.dismiss'),
-                result: {
-                    then: jasmine.createSpy('modalInstance.result.then')
-                }
-            };
-        }
+        var ctrl, modalInstance, $controller, $rootScope, $scope, HR_settings,
+            data, initController;
 
         beforeEach(module('civitasks.appDashboard'));
-
-        beforeEach(inject(function ($rootScope, $controller) {
-
-            initController = function (){
-                return $controller('ModalTaskCtrl', {
-                    $scope: $scope,
-                    $modalInstance: fakeModalInstance(),
-                    data: data,
-                    HR_settings: HR_settings
-                });
-            };
-
+        beforeEach(inject(function (_$rootScope_, _$controller_) {
+            $controller = _$controller_;
+            $rootScope = _$rootScope_;
             $scope = $rootScope.$new();
 
-            HR_settings = {
-                DATE_FORMAT: 'DD/MM/YYYY'
-            };
-
             data = {};
+            HR_settings = { DATE_FORMAT: 'DD/MM/YYYY' };
+
+            initializeCaches();
         }));
 
         describe('Task should have a default (pre-filled) date.', function () {
-
             describe('Task has not due date set.', function () {
-
                 beforeEach(function () {
-                    ctrl = initController();
+                    initController();
                 });
 
                 it('task has a default due date', function () {
@@ -65,7 +44,7 @@ define([
                     customDate = moment().subtract(7, 'days').toDate();
                     data.activity_date_time = customDate;
 
-                    ctrl = initController();
+                    initController();
                 });
 
                 it("date shouldn't be changed if task already has a due date", function () {
@@ -77,5 +56,68 @@ define([
                 });
             });
         });
+
+        describe('Lookup contacts lists', function () {
+            describe('when in "new task" mode', function () {
+                beforeEach(function () {
+                    initController();
+                });
+
+                it('has the lists empty', function () {
+                    expect($scope.contacts.assignee).toEqual([]);
+                    expect($scope.contacts.target).toEqual([]);
+                });
+            });
+
+            describe('when in "edit task" mode', function () {
+                beforeEach(function () {
+                    data = { id: '2', assignee_contact_id: '3', target_contact_id: '1' };
+                    $rootScope.cache.contact.arrSearch = cachedContacts();
+
+                    initController();
+                });
+
+                it('has the list filled with just the contacts linked to the task', function () {
+                    expect($scope.contacts.assignee).toEqual([{ id: '3' }]);
+                    expect($scope.contacts.target).toEqual([{ id: '1' }]);
+                });
+            });
+
+            /**
+             * A mocked list of cached contacts
+             *
+             * @return {Array}
+             */
+            function cachedContacts() {
+                return [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }];
+            }
+        });
+
+        function fakeModalInstance() {
+            return {
+                close: jasmine.createSpy('modalInstance.close'),
+                dismiss: jasmine.createSpy('modalInstance.dismiss'),
+                result: {
+                    then: jasmine.createSpy('modalInstance.result.then')
+                }
+            };
+        }
+
+        function initController () {
+            ctrl = $controller('ModalTaskCtrl', {
+                $rootScope: $rootScope,
+                $scope: $scope,
+                $modalInstance: fakeModalInstance(),
+                data: data,
+                HR_settings: HR_settings
+            });
+        };
+
+        function initializeCaches() {
+            $rootScope.cache = {
+                assignment: { arrSearch: [] },
+                contact: { arrSearch: [] }
+            };
+        }
     });
 });
