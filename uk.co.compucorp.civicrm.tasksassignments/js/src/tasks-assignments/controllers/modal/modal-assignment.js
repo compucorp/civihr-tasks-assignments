bd2b8086241c6efdb59c964dcb6128b24c879012
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-02-28 21:41:09 +0100

    PCHR-1923: React to watched parent props only if task is enabled

--- uk.co.compucorp.civicrm.tasksassignments/js/src/tasks-assignments/controllers/modal/modal-assignment.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/src/tasks-assignments/controllers/modal/modal-assignment.js
@@ -390,29 +390,23 @@ define([
     ]);
 
     controllers.controller('ModalAssignmentActivityCtrl', ['$scope', '$log',
-        function ($scope, $log) {
-            $log.debug('Controller: ModalAssignmentTaskCtrl');
+      function ($scope, $log) {
+        $log.debug('Controller: ModalAssignmentTaskCtrl');
 
-            $scope.isDisabled = !$scope.activity.activity_type_id && !$scope.activity.isAdded;
-            $scope.activity.create = !$scope.isDisabled;
+        $scope.isDisabled = !$scope.activity.activity_type_id && !$scope.activity.isAdded;
+        $scope.activity.create = !$scope.isDisabled;
 
-            if ($scope.isDisabled) {
-                return
-            }
+        $scope.$watch('$parent.assignment.dueDate', function (assignmentDueDate) {
+          if ($scope.activity.create) {
+            $scope.activity.activity_date_time = !!assignmentDueDate ? moment(assignmentDueDate).add($scope.activity.offset, 'days').toDate() : null;
+          }
+        });
 
-            $scope.$watch('$parent.assignment.dueDate', function (assignmentDueDate) {
-                $scope.activity.activity_date_time = !!assignmentDueDate ? moment(assignmentDueDate).add($scope.activity.offset, 'days').toDate() : null;
-            });
-
-            $scope.$watch('$parent.assignment.contact_id', function (targetContactId) {
-
-                if (!targetContactId) {
-                    return
-                }
-
-                $scope.activity.target_contact_id = [targetContactId];
-            });
-
-        }
+        $scope.$watch('$parent.assignment.contact_id', function (targetContactId) {
+          if (targetContactId && $scope.activity.create) {
+            $scope.activity.target_contact_id = [targetContactId];
+          }
+        });
+      }
     ]);
 });
