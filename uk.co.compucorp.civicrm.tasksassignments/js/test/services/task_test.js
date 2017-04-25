cc2b0f8b4cd62464d9f75ab0744eda68a337ce56
Buddha Man Nepali <bmnepali980@gmail.com>
2017-04-25 16:23:53 +0545

    PCHR-2139: Update test cases for new changes

--- uk.co.compucorp.civicrm.tasksassignments/js/test/services/task_test.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/services/task_test.js
@@ -8,10 +8,11 @@ define([
   var taskMock = taskMock;
   var fakeTask = taskFabricator.single();
   var fakeTaskList  = taskFabricator.list();
+  var fakeActivityTypesOptions  = taskFabricator.activityTypesOptions();
+  var fakeStatusTypesOptions  = taskFabricator.statusIdFieldsOptions();
   var fakeReminderNote  = taskFabricator.reminderNote();
 
   var mockBackendCalls = function ($httpBackend) {
-    $httpBackend.whenGET(/action=getoptions&debug=true&entity=Task/).respond({});
     $httpBackend.whenGET(/action=getoptions&entity=Document/).respond({});
     $httpBackend.whenGET(/action=get&entity=CaseType/).respond({});
     $httpBackend.whenGET(/action=get&entity=contact/).respond({});
@@ -27,12 +28,13 @@ define([
     beforeEach(inject(function (_Task_, _$httpParamSerializer_, _$httpBackend_) {
       Task = _Task_;
       $httpBackend = _$httpBackend_;
-      $httpParamSerializer = _$httpParamSerializer_
+      $httpParamSerializer = _$httpParamSerializer_;
 
       spyOn(Task, 'save').and.callThrough();
     }));
 
     beforeEach(function () {
+      $httpBackend.whenGET(/action=getoptions&debug=true&entity=Task/).respond({});
       mockBackendCalls($httpBackend);
     });
 
@@ -73,13 +75,16 @@ define([
   });
 
   describe('TaskService', function () {
-    var TaskService, $httpBackend;
+    var Task,TaskService, $httpBackend, $q, deferred;
 
     beforeEach(module('civitasks.appDashboard'));
 
-    beforeEach(inject(function (_TaskService_, _$httpBackend_) {
+    beforeEach(inject(function ( _TaskService_, _$httpBackend_, _$q_) {
       TaskService = _TaskService_;
       $httpBackend = _$httpBackend_;
+      $q = _$q_;
+      deferred = _$q_.defer();
+
     }));
 
     beforeEach(function () {
@@ -88,6 +93,7 @@ define([
 
     describe("save()", function () {
       beforeEach(function () {
+        $httpBackend.whenGET(/action=getoptions&debug=true&entity=Task/).respond({});
         $httpBackend.whenPOST(/action=create&debug=true&entity=Task/).respond(function () {
           return [200, taskMock.onSave];
         });
@@ -104,6 +110,7 @@ define([
 
     describe("assign()", function () {
       beforeEach(function () {
+        $httpBackend.whenGET(/action=getoptions&debug=true&entity=Task/).respond({});
         $httpBackend.whenPOST(/action=copy_to_assignment&debug=true&entity=Task/).respond(function () {
           return [200, taskMock.onAssign];
         });
@@ -120,6 +127,7 @@ define([
 
     describe("saveMultiple()", function () {
       beforeEach(function() {
+        $httpBackend.whenGET(/action=getoptions&debug=true&entity=Task/).respond({});
         $httpBackend.whenPOST(/action=create_multiple&debug=true&entity=Task/).respond(function () {
           return [200, taskMock.onSaveMultiple];
         });
@@ -136,6 +144,7 @@ define([
 
     describe("sendReminder()", function () {
       beforeEach(function () {
+        $httpBackend.whenGET(/action=getoptions&debug=true&entity=Task/).respond({});
         $httpBackend.whenPOST(/action=sendreminder&debug=true&entity=Task/).respond(function () {
           return [200, taskMock.onSendReminder];
         });
@@ -144,6 +153,46 @@ define([
       it('returns reminder sent status on sending reminder for a task', function () {
         TaskService.sendReminder(1, fakeReminderNote).then(function (data) {
           expect(data.values).toEqual(true);
+        });
+
+        $httpBackend.flush();
+      });
+    });
+
+    describe("getActivityTypeIdOption()", function () {
+      beforeEach(function () {
+        $httpBackend.whenGET(/action=getoptions&debug=true&entity=Task/).respond(function () {
+          return [200, taskMock.onGetOptions.activityTypeIdFields];
+        });
+      });
+
+      it("returns promise with array of list of activity types options", function () {
+        $q.all({
+          taskType: TaskService.getActivityTypeIdOption()
+        }).then(function(options) {
+          expect(options.taskType).toBeDefined();
+          expect(options.taskType.obj).toBeDefined();
+          expect(options.taskType.arr).toEqual(fakeActivityTypesOptions);
+        });
+
+        $httpBackend.flush();
+      });
+    });
+
+    describe("getStatusIdOption()", function () {
+      beforeEach(function () {
+        $httpBackend.whenGET(/action=getoptions&debug=true&entity=Task/).respond(function () {
+          return [200, taskMock.onGetOptions.statusIdFields];
+        });
+      });
+
+      it("returns promise with array of list of statys types options", function () {
+        $q.all({
+          taskStatus: TaskService.getStatusIdOption()
+        }).then(function(options) {
+          expect(options.taskStatus).toBeDefined();
+          expect(options.taskStatus.obj).toBeDefined();
+          expect(options.taskStatus.arr).toEqual(fakeStatusTypesOptions);
         });
 
         $httpBackend.flush();
