e7bf811ee75b670d47061cd36c3c0e0786568caa
Buddha Man Nepali <bmnepali980@gmail.com>
2017-05-02 20:14:04 +0545

    PCHR-2153: Reformat document.js to 2 spaced indentation

--- uk.co.compucorp.civicrm.tasksassignments/js/src/tasks-assignments/controllers/document.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/src/tasks-assignments/controllers/document.js
@@ -1,21 +1,21 @@
 define([
-    'common/moment',
-    'tasks-assignments/controllers/controllers',
-    'tasks-assignments/services/document'
-], function (moment, controllers) {
-    'use strict';
+  'common/moment',
+  'tasks-assignments/controllers/controllers',
+  'tasks-assignments/services/document'
+], function(moment, controllers) {
+  'use strict';
 
-    controllers.controller('DocumentCtrl',['$scope', '$rootScope','config', '$log',
-        function ($scope, $rootScope, config, $log) {
-            $log.debug('Controller: DocumentCtrl');
+  controllers.controller('DocumentCtrl', ['$scope', '$rootScope', 'config', '$log',
+    function($scope, $rootScope, config, $log) {
+      $log.debug('Controller: DocumentCtrl');
 
-            $scope.document.activity_date_time = !!$scope.document.activity_date_time ? moment($scope.document.activity_date_time).toDate() : null;
-            $scope.document.expire_date = !!$scope.document.expire_date ? moment($scope.document.expire_date).toDate() : null;
-            $scope.urlFile = config.url.FILE+'/zip?entityID='+$scope.document.id+'&entityTable=civicrm_activity';
+      $scope.document.activity_date_time = !!$scope.document.activity_date_time ? moment($scope.document.activity_date_time).toDate() : null;
+      $scope.document.expire_date = !!$scope.document.expire_date ? moment($scope.document.expire_date).toDate() : null;
+      $scope.urlFile = config.url.FILE + '/zip?entityID=' + $scope.document.id + '&entityTable=civicrm_activity';
 
-            $scope.$watch('document.activity_date_time',function(documentDateTime){
-                $scope.document.due = new Date(documentDateTime).setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0);
-            });
-
-        }]);
+      $scope.$watch('document.activity_date_time', function(documentDateTime) {
+        $scope.document.due = new Date(documentDateTime).setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0);
+      });
+    }
+  ]);
 });
