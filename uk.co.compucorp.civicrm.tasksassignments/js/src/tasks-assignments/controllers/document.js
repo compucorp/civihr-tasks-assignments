5b6cfbe1d29b6cc34e1ef7d7a2154ebec5220526
Buddha Man Nepali <bmnepali980@gmail.com>
2017-05-02 20:17:53 +0545

    PCHR-2153: Modify valid from date and remind me value

--- uk.co.compucorp.civicrm.tasksassignments/js/src/tasks-assignments/controllers/document.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/src/tasks-assignments/controllers/document.js
@@ -11,6 +11,8 @@ define([
 
       $scope.document.activity_date_time = !!$scope.document.activity_date_time ? moment($scope.document.activity_date_time).toDate() : null;
       $scope.document.expire_date = !!$scope.document.expire_date ? moment($scope.document.expire_date).toDate() : null;
+      $scope.document.valid_from = !!$scope.document.valid_from ? moment($scope.document.valid_from).toDate() : null;
+      $scope.document.remind_me = $scope.document.remind_me == 1 ? true : false;
       $scope.urlFile = config.url.FILE + '/zip?entityID=' + $scope.document.id + '&entityTable=civicrm_activity';
 
       $scope.$watch('document.activity_date_time', function(documentDateTime) {
