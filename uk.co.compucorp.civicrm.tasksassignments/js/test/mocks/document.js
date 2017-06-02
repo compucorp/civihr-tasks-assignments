60dbedd992711457805c87cfa415b5ec06530299
Buddha Man Nepali <bmnepali980@gmail.com>
2017-06-02 22:40:31 +0545

    PCHR-2144: Add document mock data

---
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/mocks/document.js
@@ -0,0 +1,27 @@
+/* globals define */
+
+define(function () {
+  return {
+    document: {
+      id: '1216',
+      activity_date_time: '2017-06-09T18:15:00.000Z',
+      activity_type_id: '121',
+      status_id: '3',
+      assignee_contact_id: ['203'],
+      source_contact_id: '205',
+      target_contact_id: ['203'],
+      custom_83_55: '2017-06-02 00:00:00',
+      custom_100013_55: '2017-05-30 00:00:00',
+      custom_100014_55: '1',
+      custom_100015_55: '999999922',
+      case_id: '',
+      file_count: '2',
+      expire_date: '2017-06-01T18:15:00.000Z',
+      valid_from: '2017-05-29T18:15:00.000Z',
+      remind_me: true,
+      document_number: '999999922',
+      $$hashKey: 'object:182',
+      due: false
+    }
+  };
+});
