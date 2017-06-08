028f46d17362bda2728901a0bb6fcb5cbf27a001
Buddha Man Nepali <bmnepali980@gmail.com>
2017-06-08 21:50:16 +0545

    PCHR-2146: Fix indentation issues + pre-commit issues

--- uk.co.compucorp.civicrm.tasksassignments/js/test/mocks/data/assignment.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/mocks/data/assignment.js
@@ -1,160 +1,161 @@
-define(function () {
-    return {
-        assignment: {
-            case_type_id: "5",
-            client_id: "20",
-            contact_id: "20",
-            dueDate: new Date(),
-            start_date: new Date(),
-            status_id: "1",
-            subject: "Joining"
-        },
-        taskList: [
-            {
-                activity_date_time: "",
-                activity_type_id: "71",
-                assignee_contact_id: [],
-                case_id: "21",
-                create: true,
-                isAdded: false,
-                name: "Schedule joining date",
-                offset: "0",
-                source_contact_id: "205",
-                status_id: "1",
-                target_contact_id: []
-            },
-            {
-                activity_date_time: "",
-                activity_type_id: "71",
-                assignee_contact_id: [],
-                case_id: "21",
-                create: true,
-                isAdded: false,
-                name: "Schedule joining date",
-                offset: "0",
-                source_contact_id: "205",
-                status_id: "1",
-                target_contact_id: []
-            },
-            {
-                activity_date_time: "",
-                activity_type_id: "71",
-                assignee_contact_id: [],
-                case_id: "21",
-                create: true,
-                isAdded: false,
-                name: "Schedule joining date",
-                offset: "0",
-                source_contact_id: "205",
-                status_id: "1",
-                target_contact_id: []
-            },
-            {
-                activity_date_time: "",
-                activity_type_id: "71",
-                assignee_contact_id: [],
-                case_id: "21",
-                create: true,
-                isAdded: false,
-                name: "Schedule joining date",
-                offset: "0",
-                source_contact_id: "205",
-                status_id: "1",
-                target_contact_id: []
-            },
-            {
-                activity_date_time: "",
-                activity_type_id: "71",
-                assignee_contact_id: [],
-                case_id: "21",
-                create: true,
-                isAdded: false,
-                name: "Schedule joining date",
-                offset: "0",
-                source_contact_id: "205",
-                status_id: "1",
-                target_contact_id: []
-            }
-        ],
-        documentList: [
-            {
-                activity_date_time: new Date(),
-                activity_type_id: "130",
-                assignee_contact_id: [],
-                case_id: "21",
-                create: true,
-                id: "823",
-                isAdded: true,
-                name: null,
-                offset: 0,
-                source_contact_id: "205",
-                status_id: "1",
-                target_contact_id: []
-            },
-            {
-                activity_date_time: new Date(),
-                activity_type_id: "130",
-                assignee_contact_id: [],
-                case_id: "21",
-                create: true,
-                id: "823",
-                isAdded: true,
-                name: null,
-                offset: 0,
-                source_contact_id: "205",
-                status_id: "1",
-                target_contact_id: []
-            },
-            {
-                activity_date_time: new Date(),
-                activity_type_id: "130",
-                assignee_contact_id: [],
-                case_id: "21",
-                create: true,
-                id: "823",
-                isAdded: true,
-                name: null,
-                offset: 0,
-                source_contact_id: "205",
-                status_id: "1",
-                target_contact_id: []
-            },
-            {
-                activity_date_time: new Date(),
-                activity_type_id: "130",
-                assignee_contact_id: [],
-                case_id: "21",
-                create: true,
-                id: "823",
-                isAdded: true,
-                name: null,
-                offset: 0,
-                source_contact_id: "205",
-                status_id: "1",
-                target_contact_id: []
-            },
+/* eslint-env amd */
 
-        ],
-        timeline: [
+define(function () {
+  return {
+    assignment: {
+      case_type_id: '5',
+      client_id: '20',
+      contact_id: '20',
+      dueDate: new Date(),
+      start_date: new Date(),
+      status_id: '1',
+      subject: 'Joining'
+    },
+    taskList: [
+      {
+        activity_date_time: '',
+        activity_type_id: '71',
+        assignee_contact_id: [],
+        case_id: '21',
+        create: true,
+        isAdded: false,
+        name: 'Schedule joining date',
+        offset: '0',
+        source_contact_id: '205',
+        status_id: '1',
+        target_contact_id: []
+      },
+      {
+        activity_date_time: '',
+        activity_type_id: '71',
+        assignee_contact_id: [],
+        case_id: '21',
+        create: true,
+        isAdded: false,
+        name: 'Schedule joining date',
+        offset: '0',
+        source_contact_id: '205',
+        status_id: '1',
+        target_contact_id: []
+      },
+      {
+        activity_date_time: '',
+        activity_type_id: '71',
+        assignee_contact_id: [],
+        case_id: '21',
+        create: true,
+        isAdded: false,
+        name: 'Schedule joining date',
+        offset: '0',
+        source_contact_id: '205',
+        status_id: '1',
+        target_contact_id: []
+      },
+      {
+        activity_date_time: '',
+        activity_type_id: '71',
+        assignee_contact_id: [],
+        case_id: '21',
+        create: true,
+        isAdded: false,
+        name: 'Schedule joining date',
+        offset: '0',
+        source_contact_id: '205',
+        status_id: '1',
+        target_contact_id: []
+      },
+      {
+        activity_date_time: '',
+        activity_type_id: '71',
+        assignee_contact_id: [],
+        case_id: '21',
+        create: true,
+        isAdded: false,
+        name: 'Schedule joining date',
+        offset: '0',
+        source_contact_id: '205',
+        status_id: '1',
+        target_contact_id: []
+      }
+    ],
+    documentList: [
+      {
+        activity_date_time: new Date(),
+        activity_type_id: '130',
+        assignee_contact_id: [],
+        case_id: '21',
+        create: true,
+        id: '823',
+        isAdded: true,
+        name: null,
+        offset: 0,
+        source_contact_id: '205',
+        status_id: '1',
+        target_contact_id: []
+      },
+      {
+        activity_date_time: new Date(),
+        activity_type_id: '130',
+        assignee_contact_id: [],
+        case_id: '21',
+        create: true,
+        id: '823',
+        isAdded: true,
+        name: null,
+        offset: 0,
+        source_contact_id: '205',
+        status_id: '1',
+        target_contact_id: []
+      },
+      {
+        activity_date_time: new Date(),
+        activity_type_id: '130',
+        assignee_contact_id: [],
+        case_id: '21',
+        create: true,
+        id: '823',
+        isAdded: true,
+        name: null,
+        offset: 0,
+        source_contact_id: '205',
+        status_id: '1',
+        target_contact_id: []
+      },
+      {
+        activity_date_time: new Date(),
+        activity_type_id: '130',
+        assignee_contact_id: [],
+        case_id: '21',
+        create: true,
+        id: '823',
+        isAdded: true,
+        name: null,
+        offset: 0,
+        source_contact_id: '205',
+        status_id: '1',
+        target_contact_id: []
+      }
+    ],
+    timeline: [
+      {
+        name: 'timeline_1',
+        label: 'Timeline',
+        timeline: '1',
+        activityTypes: [
           {
-            name: "timeline_1",
-            label: "Timeline",
-            timeline: "1",
-            activityTypes: [
-              {
-                name: "Start Probation workflow",
-                status: "Scheduled",
-                reference_activity: "Open Case",
-                reference_offset: "1",
-                reference_select: "newest"
-              }
-            ]
-          },
-          {
-            name: "timeline_2",
-            label: "Timeline 2",
-            timeline: "2"
+            name: 'Start Probation workflow',
+            status: 'Scheduled',
+            reference_activity: 'Open Case',
+            reference_offset: '1',
+            reference_select: 'newest'
           }
         ]
-    }
+      },
+      {
+        name: 'timeline_2',
+        label: 'Timeline 2',
+        timeline: '2'
+      }
+    ]
+  };
 });
