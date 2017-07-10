1ff8bda103ac7a64df8fec9afee2d3672e212bc7
Buddha Man Nepali <bmnepali980@gmail.com>
2017-07-10 20:51:16 +0545

    PCHR-2357: Update contact mock data

---
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/mocks/fabricators/contact.js
@@ -0,0 +1,45 @@
+/* eslint-env amd */
+
+define(function() {
+  return {
+    single: function() {
+      return {
+        contact_id: '3',
+        contact_type: 'Individual',
+        display_name: 'Mrs. Teresa Jacobs',
+        email: 'teresaj@mymail.co.nz',
+        email_id: '16',
+        hrjobroles_id: '',
+        id: '3',
+        sort_name: 'Jacobs, Teresa',
+        description: []
+      };
+    },
+    list: function() {
+      return [
+        {
+          contact_id: '3',
+          contact_type: 'Individual',
+          display_name: 'Mrs. Teresa Jacobs',
+          email: 'teresaj@mymail.co.nz',
+          email_id: '16',
+          hrjobroles_id: '',
+          id: '3',
+          sort_name: 'Jacobs, Teresa',
+          description: []
+        },
+        {
+          contact_id: '4',
+          contact_type: 'Individual',
+          display_name: 'Felisha Yadav-Roberts',
+          email: 'felishayadav-roberts@notmail.co.in',
+          email_id: '124',
+          hrjobroles_id: '',
+          id: '4',
+          sort_name: 'Yadav-Roberts, Felisha',
+          description: []
+        }
+      ];
+    }
+  };
+});
