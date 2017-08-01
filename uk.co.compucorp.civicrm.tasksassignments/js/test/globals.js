2276dfaddb80193e2153daabdf9b0deacab723ab
Buddha Man Nepali <bmnepali980@gmail.com>
2017-08-01 08:58:05 +0545

    PCHR-2379: Add global Drupal js object for tests

--- uk.co.compucorp.civicrm.tasksassignments/js/test/globals.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/globals.js
@@ -1,21 +1,34 @@
-(function (CRM) {
-    CRM._.assign(CRM, {
-        debug: 1,
-        contactId: null,
-        adminId: 205,
-        tasksAssignments: {
-            case_extension: true,
-            extensionPath: '/base/tools/extensions/civihr_tasks/uk.co.compucorp.civicrm.tasksassignments',
-            permissions: { delete_tasks_and_documents: true },
-            settings: { documents_tab: 1 },
-            settings: { keydates_tab: 1 },
-            settings: { add_assignment_button_title: undefined },
-            settings: { is_task_dashboard_default: 1 }
-        }
-    });
+/* globals Drupal:true */
 
-    CRM.url({
-        back: '/index.php?q=*path*&*query*',
-        front: '/index.php?q=*path*&*query*'
-    });
-})(CRM);
+// Create  a global Drupal object only for testing purpose
+Drupal = {};
+
+(function (CRM, Drupal) {
+  'use strict';
+
+  CRM._.assign(CRM, {
+    debug: 1,
+    contactId: null,
+    adminId: 205,
+    tasksAssignments: {
+      case_extension: true,
+      extensionPath: '/base/tools/extensions/civihr_tasks/uk.co.compucorp.civicrm.tasksassignments',
+      permissions: { delete_tasks_and_documents: true },
+      settings: {
+        documents_tab: 1,
+        keydates_tab: 1,
+        add_assignment_button_title: undefined,
+        is_task_dashboard_default: 1
+      }
+    }
+  });
+
+  CRM.url({
+    back: '/index.php?q=*path*&*query*',
+    front: '/index.php?q=*path*&*query*'
+  });
+
+  Drupal.settings = {
+    currentCiviCRMUserId: 202
+  };
+})(CRM, Drupal);
