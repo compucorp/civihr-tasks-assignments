dad4c242ee2fab793e1b60394ed622da6a0dc733
Buddha Man Nepali <bmnepali980@gmail.com>
2017-08-04 22:38:57 +0545

    PCHR-2473: Prefix fields function by &

--- uk.co.compucorp.civicrm.tasksassignments/CRM/Tasksassignments/DAO/Assignment.php
+++ uk.co.compucorp.civicrm.tasksassignments/CRM/Tasksassignments/DAO/Assignment.php
@@ -3,7 +3,7 @@
 require_once 'CRM/Core/DAO.php';
 require_once 'CRM/Utils/Type.php';
 class CRM_Tasksassignments_DAO_Assignment extends CRM_Case_BAO_Case {
-  public static function fields() {
+  public static function &fields() {
     if (!isset(Civi::$statics[__CLASS__]['fields'])) {
       Civi::$statics[__CLASS__]['fields'] = parent::fields();
       Civi::$statics[__CLASS__]['fields']['id'] = Civi::$statics[__CLASS__]['fields']['case_id'];
