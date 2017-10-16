3a4181ce9c25d4add44903791dd3802e8dad121d
Buddha Man Nepali <bmnepali980@gmail.com>
2017-10-16 18:22:26 +0545

    PCHR-2454: Implements PR review feedbacks

--- uk.co.compucorp.civicrm.tasksassignments/js/test/controllers/document-list.spec.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/controllers/document-list.spec.js
@@ -326,11 +326,13 @@ define([
         initController();
       });
 
-      it('does not highlight the select dates filter section', function () {
-        expect(controller.isDocumentSection).toBe(false);
+      describe('before applySidebarFilters() is called', function () {
+        it('does not highlight the select dates filter section', function () {
+          expect(controller.isDocumentSection).toBe(false);
+        });
       });
 
-      describe('called applySidebarFilters()', function () {
+      describe('after applySidebarFilters() is called', function () {
         beforeEach(function () {
           controller.filterParamsHolder.dateRange.from = moment().startOf('day').toDate();
           controller.filterParamsHolder.dateRange.until = moment().add(2, 'month').startOf('day').toDate();
