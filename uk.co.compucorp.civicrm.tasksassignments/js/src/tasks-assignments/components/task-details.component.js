5dcbf82946a159eaae5a56e983a6bf6491c2363e
René Olivo <minet.ws@gmail.com>
2018-01-11 22:10:10 -0400

    PCHR-3058: update task when fields change

--- uk.co.compucorp.civicrm.tasksassignments/js/src/tasks-assignments/components/task-details.component.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/src/tasks-assignments/components/task-details.component.js
@@ -28,10 +28,12 @@ define(function () {
   };
 
   TaskDetailsController.__name = 'TaskDetailsComponent';
-  TaskDetailsController.$inject = ['$log', 'config'];
+  TaskDetailsController.$inject = ['$log', 'config', 'taskService'];
 
-  function TaskDetailsController ($log, config) {
+  function TaskDetailsController ($log, config, taskService) {
     var vm = this;
+
+    vm.updateTask = updateTask;
 
     (function init () {
       vm.CONTACTS_URL = config.url.CONTACT;
@@ -46,6 +48,17 @@ define(function () {
       if (vm.task && vm.task.activity_date_time) {
         vm.task.activity_date_time = new Date(vm.task.activity_date_time);
       }
+    }
+
+    /**
+     * Updates the task using the API service.
+     */
+    function updateTask () {
+      return taskService
+        .save(vm.task)
+        .catch(function (reason) {
+          CRM.alert(reason, 'Error', 'error');
+        });
     }
   }
 
--- uk.co.compucorp.civicrm.tasksassignments/js/test/components/task-details.component.spec.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/components/task-details.component.spec.js
@@ -6,17 +6,43 @@ define([
   'use strict';
 
   describe('TaskDetailsComponent', function () {
-    var $componentController, config, ctrl;
+    var $componentController, $q, $rootScope, config, ctrl, taskService;
 
     beforeEach(module('tasks-assignments.dashboard'));
 
-    beforeEach(inject(function (_$componentController_, _config_) {
+    beforeEach(inject(function (_$componentController_, _$q_, _$rootScope_,
+      $httpBackend, _config_, _taskService_) {
       $componentController = _$componentController_;
+      $q = _$q_;
+      $rootScope = _$rootScope_;
       config = _config_;
+      taskService = _taskService_;
+
+      // @TODO remove when API mocks are available.
+      // This supresses all HTTP requests errors:
+      $httpBackend.when('GET', /\/index.php/).respond({});
     }));
 
     beforeEach(function () {
-      ctrl = $componentController('taskDetails');
+      var task = {
+        'id': '625',
+        'activity_date_time': '2017-03-15T03:00:00.000Z',
+        'activity_type_id': '13',
+        'details': '<p>Test</p>',
+        'subject': 'test',
+        'status_id': '1',
+        'assignee_contact_id': ['6'],
+        'source_contact_id': '205',
+        'target_contact_id': ['84'],
+        'case_id': '',
+        'resolved': false,
+        'completed': false,
+        'due': true
+      };
+
+      ctrl = $componentController('taskDetails', null, {
+        task: task
+      });
     });
 
     it('is defined', function () {
@@ -48,6 +74,36 @@ define([
 
       it('converts the activity string date into a Date object', function () {
         expect(ctrl.task.activity_date_time).toEqual(expectedDate);
+      });
+    });
+
+    describe('updateTask()', function () {
+      describe('when saving the task succeeds', function () {
+        beforeEach(function () {
+          spyOn(taskService, 'save').and.callThrough();
+
+          ctrl.updateTask();
+        });
+
+        it('saves the task', function () {
+          expect(taskService.save).toHaveBeenCalledWith(ctrl.task);
+        });
+      });
+
+      describe('when saving the task fails', function () {
+        var errorMessage = 'error message';
+
+        beforeEach(function () {
+          spyOn(taskService, 'save').and.returnValue($q.reject(errorMessage));
+          spyOn(CRM, 'alert');
+
+          ctrl.updateTask();
+          $rootScope.$digest();
+        });
+
+        it('displays an error message', function () {
+          expect(CRM.alert).toHaveBeenCalledWith(errorMessage, 'Error', 'error');
+        });
       });
     });
   });
