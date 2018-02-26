e80b8f9e011edfa3fa1efccdaed72cb3660b789f
Samson Alajede <ajesamson@yahoo.com>
2018-02-26 15:57:53 +0100

    PCHR-3353: Set custom field group that should not be editable to is_reserved yes

--- uk.co.compucorp.civicrm.tasksassignments/CRM/Tasksassignments/Upgrader.php
+++ uk.co.compucorp.civicrm.tasksassignments/CRM/Tasksassignments/Upgrader.php
@@ -712,6 +712,21 @@ class CRM_Tasksassignments_Upgrader extends CRM_Tasksassignments_Upgrader_Base
     return TRUE;
   }
 
+    /**
+     * Upgrade CustomGroup, setting Probation and Activity_Custom_Fields to is_reserved Yes
+     * @return bool
+     */
+  public function upgrade_1032() {
+      $result = civicrm_api3('CustomGroup', 'get', [
+          'sequential' => 1,
+          'return' => ["id"],
+          'name' => ['IN' => ["Probation", "Activity_Custom_Fields"]],
+          'api.CustomGroup.create' => ['id' => "\$value.id", 'is_reserved' => 1],
+      ]);
+
+      return true;
+  }
+
   public function uninstall() {
     CRM_Core_DAO::executeQuery("DELETE FROM `civicrm_navigation` WHERE name IN ('tasksassignments', 'ta_dashboard_tasks', 'ta_dashboard_documents', 'ta_dashboard_calendar', 'ta_dashboard_keydates', 'tasksassignments_administer', 'ta_settings')");
     CRM_Core_BAO_Navigation::resetNavigation();
