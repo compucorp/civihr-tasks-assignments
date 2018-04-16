a5ef9f5797ba7647fc16e191c1d6f6eae7751c0b
Debarshi Bhaumik <deb1990@gmail.com>
2018-04-16 12:08:10 +0530

    PCHR-3529: Unit tests

---
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/crm-tasks-workflows/karma.conf.js
@@ -0,0 +1,45 @@
+var cv = require('civicrm-cv')({ mode: 'sync' });
+
+module.exports = function (config) {
+  var civicrmPath = cv("path -d '[civicrm.root]'")[0].value;
+  var extPath = cv('path -x uk.co.compucorp.civicrm.tasksassignments')[0].value;
+
+  config.set({
+    basePath: civicrmPath,
+    browsers: ['ChromeHeadless'],
+    frameworks: ['jasmine'],
+    files: [
+      // the global dependencies
+      'bower_components/angular/angular.min.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+
+      { pattern: extPath + '/js/src/crm-tasks-workflows/modules/*.js' },
+      { pattern: extPath + '/js/src/crm-tasks-workflows/controllers/*.js' },
+      { pattern: extPath + '/js/src/crm-tasks-workflows.js' },
+
+      // the mocked components files
+      { pattern: extPath + '/js/test/crm-tasks-workflows/mocks/modules/*.js' },
+      { pattern: extPath + '/js/test/crm-tasks-workflows/mocks/services/*.js' },
+      { pattern: extPath + '/js/test/crm-tasks-workflows/mocks/data/*.js' },
+
+      // the test files
+      { pattern: extPath + '/js/test/crm-tasks-workflows/**/*.spec.js' }
+    ],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
+    junitReporter: {
+      outputDir: extPath + '/test-reports',
+      useBrowserName: false,
+      outputFile: 'karma.xml'
+    }
+  });
+};
---
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/crm-tasks-workflows/mocks/data/crm-api.data.js
@@ -0,0 +1,7 @@
+/* global angular */
+
+(function () {
+  angular.module('crm-tasks-workflows.mocks').constant('crmApiData', [
+    { name: 'Jon Snow', label: 'Game of Thrones', icon: 'wolf' }
+  ]);
+})();
