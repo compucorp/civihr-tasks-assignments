591abb3b0fec7a23d431c3a7cd13236669c8892c
Debarshi Bhaumik <deb1990@gmail.com>
2018-04-17 16:51:41 +0530

    PCHR-3529: PR Feedback changes

--- uk.co.compucorp.civicrm.tasksassignments/js/src/crm-tasks-workflows/configs/crm-case-type.config.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/src/crm-tasks-workflows/configs/crm-case-type.config.js
@@ -11,6 +11,21 @@
 
       function routeDecorator ($delegate) {
         $delegate.routes['/caseType/:id'].controller = 'CaseTypeExtendedController';
+        $delegate.routes['/caseType/:id'].resolve.activityOptions = activityOptions;
+
+        activityOptions.$inject = ['crmApi'];
+
+        function activityOptions (crmApi) {
+          return crmApi('OptionValue', 'get', {
+            option_group_id: 'activity_type',
+            component_id: { 'IN': ['CiviTask', 'CiviDocument'] },
+            sequential: 1,
+            options: {
+              sort: 'name',
+              limit: 0
+            }
+          });
+        }
 
         return $delegate;
       }
--- uk.co.compucorp.civicrm.tasksassignments/js/test/crm-tasks-workflows/mocks/data/activity-options.data.js
+++ uk.co.compucorp.civicrm.tasksassignments/js/test/crm-tasks-workflows/mocks/data/activity-options.data.js
@@ -1,7 +1,7 @@
 /* global angular */
 
 (function () {
-  angular.module('crm-tasks-workflows.mocks').constant('crmApiData', [
+  angular.module('crm-tasks-workflows.mocks').constant('activityOptionsData', [
     { name: 'Jon Snow', label: 'Game of Thrones', icon: 'wolf' }
   ]);
 })();
