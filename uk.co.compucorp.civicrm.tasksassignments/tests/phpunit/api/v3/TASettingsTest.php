ce858eca5c0bb1698a814a84c61db08683e3ca2d
Davi Alexandre <davi@davialexandre.com.br>
2018-07-05 14:35:21 -0300

    PCHR-3429: Add tests for the TASettings API

---
+++ uk.co.compucorp.civicrm.tasksassignments/tests/phpunit/api/v3/TASettingsTest.php
@@ -0,0 +1,98 @@
+<?php
+
+use CRM_Tasksassignments_Test_BaseHeadlessTest as BaseHeadlessTest;
+
+/**
+ * Class api_v3_TaskTest
+ *
+ * @group headless
+ */
+class api_v3_TASettingsTest extends BaseHeadlessTest {
+
+  private $validFields;
+  private $defaultSettings;
+
+  public function setUp() {
+    $this->validFields = $this->getValidFields();
+
+    foreach($this->validFields as $field) {
+      $this->defaultSettings[$field] = '1';
+    }
+
+    civicrm_api3('TASettings', 'set', [
+      'fields' => $this->defaultSettings
+    ]);
+  }
+
+  public function testGetReturnsAllTheSettingsWhenCallWithoutParams() {
+    $result = civicrm_api3('TASettings', 'get');
+
+    $this->assertCount(count($this->validFields), $result['values']);
+
+    foreach ($this->defaultSettings as $setting => $value) {
+      $expected = [
+        'name' => $setting,
+        'value' => $value,
+      ];
+
+      $this->assertEquals($expected, $result['values'][$setting]);
+    }
+  }
+
+  public function testGetReturnsOnlyTheSpecifiedFieldsWhenTheFieldsParamIsPassed() {
+    $field = $this->validFields[array_rand($this->validFields)];
+
+    $result = civicrm_api3('TASettings', 'get', [
+      'sequential' => 1,
+      'fields' => [$field]
+    ]);
+
+    $expected = [
+      'name' => $field,
+      'value' => $this->defaultSettings[$field]
+    ];
+
+    $this->assertEquals($expected, $result['values'][0]);
+  }
+
+  public function testSetUpdatesSettingsValuesAndReturnTheUpdatedValues() {
+    $randKeys = array_rand($this->validFields, 2);
+
+    $field1 = $this->validFields[$randKeys[0]];
+    $field2 = $this->validFields[$randKeys[1]];
+
+    $field1Value = 999999;
+    $field2Value = 888;
+
+    $result = civicrm_api3('TASettings', 'set', [
+      'fields' => [
+        $field1 => $field1Value,
+        $field2 => $field2Value
+      ]
+    ]);
+
+    $expected = [
+      $field1 => [
+        'name' => $field1,
+        'value' => $field1Value
+      ],
+      $field2 => [
+        'name' => $field2,
+        'value' => $field2Value
+      ]
+    ];
+
+    $this->assertEquals($expected, $result['values']);
+  }
+
+  private function getValidFields() {
+    $result = civicrm_api3('OptionValue', 'get', [
+      'sequential' => 1,
+      'option_group_id' => 'ta_settings',
+      'return' => ['name']
+    ]);
+
+    return array_column($result['values'], 'name');
+  }
+
+}
